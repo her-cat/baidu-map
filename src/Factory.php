e82ce85f2b9d0e4877a535f971821b9401e272a0
her-cat <i@her-cat.com>
2019-08-03 15:57:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Factory.php
+++ src/Factory.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the her-cat/baidu-map.
+ *
+ * (c) her-cat <i@her-cat.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HerCat\BaiduMap;
 
 use HerCat\BaiduMap\Kernel\Exceptions\RuntimeException;
@@ -17,7 +26,7 @@ class Factory
 
     /**
      * @param string $name
-     * @param array $config
+     * @param array  $config
      *
      * @return mixed
      *
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,18 @@
 <?php
 
+/*
+ * This file is part of the her-cat/baidu-map.
+ *
+ * (c) her-cat <i@her-cat.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HerCat\BaiduMap\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-
 }
