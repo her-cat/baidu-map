90b3e98a616dddf6b36bc6a9a64f16add961eeab
her-cat <i@her-cat.com>
2019-08-11 21:44:03 +0800

    Add Http Response.

---
+++ src/Kernel/Http/Response.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace HerCat\BaiduMap\Kernel\Http;
+
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use HerCat\BaiduMap\Kernel\Support\Collection;
+use HerCat\BaiduMap\Kernel\Support\XML;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class Response.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class Response extends GuzzleResponse
+{
+    /**
+     * @return string
+     */
+    public function getBodyContents()
+    {
+        $this->getBody()->rewind();
+        $contents = $this->getBody()->getContents();
+        $this->getBody()->rewind();
+
+        return $contents;
+    }
+
+    /**
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @return Response
+     */
+    public static function buildFromPsrResponse(ResponseInterface $response)
+    {
+        return new static(
+            $response->getStatusCode(),
+            $response->getHeaders(),
+            $response->getBody(),
+            $response->getProtocolVersion(),
+            $response->getReasonPhrase()
+        );
+    }
+
+    /**
+     * Build to json.
+     *
+     * @return string
+     */
+    public function toJson()
+    {
+        return json_encode($this->toArray());
+    }
+
+    /**
+     * Build to array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $content = $this->removeControlCharacters($this->getBodyContents());
+
+        if (false !== stripos($this->getHeaderLine('Content-Type'), 'xml') || 0 === stripos($content, '<xml')) {
+            return XML::parse($content);
+        }
+
+        $array = json_decode($content, true, 512, JSON_BIGINT_AS_STRING);
+
+        if (JSON_ERROR_NONE === json_last_error()) {
+            return (array) $array;
+        }
+
+        return [];
+    }
+
+    /**
+     * Get collection data.
+     *
+     * @return Collection
+     */
+    public function toCollection()
+    {
+        return new Collection($this->toArray());
+    }
+
+    /**
+     * @return object
+     */
+    public function toObject()
+    {
+        return json_decode($this->toJson());
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function __toString()
+    {
+        return $this->getBodyContents();
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    protected function removeControlCharacters(string $content)
+    {
+        return \preg_replace('/[\x00-\x1F\x80-\x9F]/u', '', $content);
+    }
+}
---
+++ src/Kernel/Traits/ResponseCastable.php
@@ -0,0 +1,88 @@
+<?php
+
+
+namespace HerCat\BaiduMap\Kernel\Traits;
+
+use HerCat\BaiduMap\Kernel\Contracts\Arrayable;
+use HerCat\BaiduMap\Kernel\Exceptions\InvalidArgumentException;
+use HerCat\BaiduMap\Kernel\Exceptions\InvalidConfigException;
+use HerCat\BaiduMap\Kernel\Http\Response;
+use HerCat\BaiduMap\Kernel\Support\Collection;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Trait ResponseCastable.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+trait ResponseCastable
+{
+    /**
+     * @param ResponseInterface $response
+     * @param null $type
+     *
+     * @return array|Response|Collection|object|ResponseInterface
+     *
+     * @throws InvalidConfigException
+     */
+    protected function castResponseToType(ResponseInterface $response, $type = null)
+    {
+        $response = Response::buildFromPsrResponse($response);
+        $response->getBody()->rewind();
+
+        switch ($type ?? 'array') {
+            case 'collection':
+                return $response->toCollection();
+            case 'array':
+                return $response->toArray();
+            case 'object':
+                return $response->toObject();
+            case 'raw':
+                return $response;
+            default:
+                if (!is_subclass_of($type, Arrayable::class)) {
+                    throw new InvalidConfigException(sprintf(
+                        'Config key "response_type" classname must be an instanceof %s',
+                        Arrayable::class
+                    ));
+                }
+
+                return new $type($response);
+        }
+    }
+
+    /**
+     * @param mixed $response
+     * @param string|null $type
+     *
+     * @return array|Response|Collection|object|ResponseInterface
+     *
+     * @throws InvalidConfigException
+     * @throws InvalidArgumentException
+     */
+    protected function detectAndCastResponseToType($response, $type = null)
+    {
+        switch (true) {
+            case $response instanceof ResponseInterface:
+                $response = Response::buildFromPsrResponse($response);
+
+                break;
+            case $response instanceof Arrayable:
+                $response = new Response(200, [], json_encode($response->toArray()));
+
+                break;
+            case ($response instanceof Collection) || is_array($response) || is_object($response):
+                $response = new Response(200, [], json_encode($response));
+
+                break;
+            case is_scalar($response):
+                $response = new Response(200, [], $response);
+
+                break;
+            default:
+                throw new InvalidArgumentException(sprintf('Unsupported response type "%s"', gettype($response)));
+        }
+
+        return $this->castResponseToType($response, $type);
+    }
+}
