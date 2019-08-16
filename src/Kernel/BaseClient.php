f81dc8df72533de214ce11e54e7f62be947b2d3b
her-cat <i@her-cat.com>
2019-08-16 21:47:11 +0800

    Added BaseClient.

---
+++ src/Kernel/BaseClient.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace HerCat\BaiduMap\Kernel;
+
+use Closure;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use HerCat\BaiduMap\Kernel\Http\Response;
+use HerCat\BaiduMap\Kernel\Http\StreamResponse;
+use HerCat\BaiduMap\Kernel\Traits\HasHttpRequests;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LogLevel;
+
+/**
+ * Class BaseClient.
+ *
+ * @author her-cat <i@her-cat.com>
+ */
+class BaseClient
+{
+    use HasHttpRequests {request as performRequest;}
+
+    /**
+     * @var ServiceContainer
+     */
+    protected $app;
+
+    /**
+     * @var string
+     */
+    protected $baseUri;
+
+    /**
+     * BaseClient constructor.
+     * @param ServiceContainer $app
+     */
+    public function __construct(ServiceContainer $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * @param string $url
+     * @param array $query
+     *
+     * @return array|Http\Response|Support\Collection|mixed|object|ResponseInterface
+     *
+     * @throws Exceptions\InvalidConfigException
+     * @throws GuzzleException
+     */
+    public function httpGet($url, array $query = [])
+    {
+        return $this->request($url, 'GET', ['query' => $query]);
+    }
+
+    /**
+     * @param string $url
+     * @param array $params
+     *
+     * @return array|Http\Response|Support\Collection|mixed|object|ResponseInterface
+     *
+     * @throws Exceptions\InvalidConfigException
+     * @throws GuzzleException
+     */
+    public function httpPost($url, array $params = [])
+    {
+        return $this->request($url, 'POST', ['form_params' => $params]);
+    }
+
+    /**
+     * @param string $url
+     * @param string $method
+     * @param array $params
+     *
+     * @return array|Response|Support\Collection|object|ResponseInterface
+     *
+     * @throws Exceptions\InvalidConfigException
+     * @throws GuzzleException
+     */
+    public function httpGetStream($url, $method = 'GET', array $params = [])
+    {
+        $options = ('GET' == strtoupper($method))
+            ? ['query' => $params]
+            : ['form_params' => $params];
+
+        $response = $this->request($url, $method, $options, true);
+
+        if (false !== stripos($response->getHeaderLine('Content-Type'), 'image')) {
+            return StreamResponse::buildFromPsrResponse($response);
+        }
+
+        return $this->castResponseToType($response, $this->app->config->get('response_type'));
+    }
+
+    /**
+     * @param string $url
+     * @param string $method
+     * @param array $options
+     * @param bool $returnRaw
+     *
+     * @return array|Http\Response|Support\Collection|mixed|object|ResponseInterface
+     *
+     * @throws Exceptions\InvalidConfigException
+     * @throws GuzzleException
+     */
+    public function request($url, $method = 'GET', array $options = [], $returnRaw = false)
+    {
+        if (empty($this->middlewares)) {
+            $this->registerHttpMiddlewares();
+        }
+
+        $response =$this->performRequest($url, $method, $options);
+
+        return $returnRaw ? $response : $this->castResponseToType($response, $this->app->config->get('response_type'));
+    }
+
+    /**
+     * @param string $url
+     * @param string $method
+     * @param array $options
+     *
+     * @return Response
+     *
+     * @throws Exceptions\InvalidConfigException
+     * @throws GuzzleException
+     */
+    public function requestRaw($url, $method = 'GET', array $options = [])
+    {
+        return Response::buildFromPsrResponse($this->request($url, $method, $options, true));
+    }
+
+    /**
+     * Register Guzzle middleware.
+     */
+    protected function registerHttpMiddlewares()
+    {
+        $this->pushMiddleware($this->akMiddleware(), 'ak');
+
+        if ($this->app->config->has('sk')) {
+            $this->pushMiddleware($this->signatureMiddleware(), 'signature');
+        }
+
+        $this->pushMiddleware($this->logMiddleware(), 'log');
+    }
+
+    /**
+     * Attache ak to request.
+     *
+     * @return Closure
+     */
+    protected function akMiddleware()
+    {
+        return function (callable $handler) {
+            return function (RequestInterface $request, array $options) use ($handler) {
+                $request = $this->app->signature->applyAkToRequest($request);
+
+                return $handler($request, $options);
+            };
+        };
+    }
+
+    /**
+     * Attache signature to request.
+     *
+     * @return Closure
+     */
+    protected function signatureMiddleware()
+    {
+        return function (callable $handler) {
+            return function (RequestInterface $request, array $options) use ($handler) {
+                $request = $this->app->signature->applySignatureToRequest($request);
+
+                return $handler($request, $options);
+            };
+        };
+    }
+
+    /**
+     * Log the request.
+     *
+     * @return callable
+     */
+    protected function logMiddleware()
+    {
+        $template = $this->app->config->get('http.log_template', MessageFormatter::DEBUG);
+
+        $formatter = new MessageFormatter($template);
+
+        return Middleware::log($this->app->logger, $formatter, LogLevel::DEBUG);
+    }
+}
---
+++ tests/Kernel/BaseClientTest.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace HerCat\BaiduMap\Tests\Kernel;
+
+use Closure;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use HerCat\BaiduMap\Kernel\BaseClient;
+use HerCat\BaiduMap\Kernel\ServiceContainer;
+use HerCat\BaiduMap\Tests\TestCase;
+use http\Exception\RuntimeException;
+
+class BaseClientTest extends TestCase
+{
+    public function mockClient($methods = [], ServiceContainer $app = null)
+    {
+        $methods = implode(',', (array) $methods);
+
+        return \Mockery::mock(BaseClient::class."[{$methods}]", [
+            !is_null($app) ? $app : \Mockery::mock(ServiceContainer::class),
+        ]);
+    }
+
+    public function testHttpGet()
+    {
+        $client = $this->mockClient('request');
+
+        $client->expects()->request('mock-uri', 'GET', ['query' => ['foo' => 'bar']])->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->httpGet('mock-uri', ['foo' => 'bar']));
+    }
+
+    public function testHttpPost()
+    {
+        $client = $this->mockClient('request');
+
+        $client->expects()->request('mock-uri', 'POST', ['form_params' => ['foo' => 'bar']])->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->httpPost('mock-uri', ['foo' => 'bar']));
+    }
+
+    public function testHttpGetStreamWithNonStreamResponse()
+    {
+        $app = new ServiceContainer(['response_type' => 'array']);
+
+        $client = $this->mockClient('request', $app);
+
+        $response = new Response(200, [], '{"foo":"bar"}');
+
+        $client->expects()
+            ->request('mock-url', 'GET', ['query' => ['name' => 'mock-name']], true)
+            ->andReturn($response);
+
+        $response = $client->httpGetStream('mock-url', 'GET', ['name' => 'mock-name']);
+
+        $this->assertSame(['foo' => 'bar'], $response);
+    }
+
+    public function testRequest()
+    {
+        $app = new ServiceContainer(['response_type' => 'array']);
+
+        $client = $this->mockClient(['registerHttpMiddlewares', 'performRequest'], $app)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $response = new Response(200, [], '{"foo": "bar"}');
+
+        // default value
+        $client->expects()->registerHttpMiddlewares();
+        $client->expects()->performRequest('mock-url', 'GET', [])->andReturn($response);
+        $this->assertSame(['foo' => 'bar'], $client->request('mock-url'));
+
+        // custom value with return raw response
+        $client->expects()->registerHttpMiddlewares();
+        $client->expects()->performRequest('mock-url', 'GET', ['name' => 'mock-name'])->andReturn($response);
+        $this->assertSame($response, $client->request('mock-url', 'GET', ['name' => 'mock-name'], true));
+    }
+
+    public function testRequestRaw()
+    {
+        $client = $this->mockClient('request');
+
+        $response = new Response(200, [], '{"foo":"bar"}');
+
+        $client->expects()->request('mock-url', 'GET', [], true)->andReturn($response);
+
+        $response = $client->requestRaw('mock-url', 'GET');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame('{"foo":"bar"}', $response->toJson());
+    }
+
+    public function testRegisterHttpMiddlewares()
+    {
+        $app = new ServiceContainer();
+
+        $client = $this->mockClient(['pushMiddleware', 'akMiddleware', 'signatureMiddleware', 'logMiddleware'], $app)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $akMiddleware = function () {
+            return 'ak';
+        };
+
+        $signatureMiddleware = function () {
+            return 'signature';
+        };
+
+        $logMiddleware = function () {
+            return 'log';
+        };
+
+        $client->expects()->akMiddlware()->andReturn($akMiddleware);
+        $client->expects()->signatureMiddleware()->andReturn($signatureMiddleware);
+        $client->expects()->logMiddleware()->andReturn($logMiddleware);
+        $client->expects()->pushMiddleware($akMiddleware, 'ak');
+        $client->expects()->pushMiddleware($signatureMiddleware, 'signature');
+        $client->expects()->pushMiddleware($logMiddleware, 'log');
+
+        $client->registerHttpMiddlewares();
+    }
+
+    public function testAkMiddleware()
+    {
+        $app = new ServiceContainer(['ak' => 'mock-ak']);
+
+        $client = $this->mockClient(['akMiddleware'], $app)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $func = $client->akMiddleware();
+
+        $options = ['foo' => 'bar'];
+        $request = new Request('GET', 'mock-url');
+
+        $middleware = $func(function ($request, $options) {
+            return compact('request', 'options');
+        });
+
+        $result = $middleware($request, $options);
+
+        $this->assertSame('ak=mock-ak', $result['request']->getUri()->getQuery());
+        $this->assertSame(['foo' => 'bar'], $result['options']);
+    }
+
+    public function testSignatureMiddleware()
+    {
+        $app = new ServiceContainer(['ak' => 'mock-ak', 'sk' => 'mock-sk']);
+
+        $client = $this->mockClient(['akMiddleware', 'signatureMiddleware'], $app)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $options = ['foo' => 'bar'];
+        $request = new Request('GET', 'mock-url');
+
+        $func = $client->akMiddleware();
+
+        $middleware = $func(function ($request, $options) {
+            return compact('request', 'options');
+        });
+
+        $result = $middleware($request, $options);
+
+        $func = $client->signatureMiddleware();
+
+        $middleware = $func(function ($request, $options) {
+            return compact('request', 'options');
+        });
+
+        $result = $middleware($result['request'], $result['options']);
+
+        $signature = md5(urlencode("mock-url?ak=mock-akmock-sk"));
+
+        $this->assertSame(
+            "ak=mock-ak&sn={$signature}",
+            $result['request']->getUri()->getQuery()
+        );
+
+        $this->assertSame(['foo' => 'bar'], $result['options']);
+    }
+
+    public function testLogMiddleware()
+    {
+        $app = new ServiceContainer(['ak' => 'mock-ak', 'sk' => 'mock-sk']);
+
+        $client = $this->mockClient('logMiddleware', $app)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $this->assertInstanceOf(Closure::class, $client->logMiddleware());
+    }
+}
