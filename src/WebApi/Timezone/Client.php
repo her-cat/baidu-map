bc17a03bbe08aaf5261018502bebf7acf34ae654
her-cat <i@her-cat.com>
2019-08-19 13:53:45 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/WebApi/Timezone/Client.php
+++ src/WebApi/Timezone/Client.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the her-cat/baidu-map.
+ *
+ * (c) her-cat <i@her-cat.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HerCat\BaiduMap\WebApi\Timezone;
 
 use GuzzleHttp\Exception\GuzzleException;
@@ -19,8 +28,8 @@ class Client extends BaseClient
     /**
      * @param string|float $longitude
      * @param string|float $latitude
-     * @param int|null $timestamp
-     * @param string $coordinateType
+     * @param int|null     $timestamp
+     * @param string       $coordinateType
      *
      * @return array|Response|Collection|mixed|object|ResponseInterface
      *
