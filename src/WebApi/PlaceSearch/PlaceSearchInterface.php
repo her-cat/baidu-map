884f0bc37dc43f7d976a15f45a0185f3f24e5c54
her-cat <i@her-cat.com>
2019-08-26 11:59:01 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/WebApi/PlaceSearch/PlaceSearchInterface.php
+++ src/WebApi/PlaceSearch/PlaceSearchInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the her-cat/baidu-map.
+ *
+ * (c) her-cat <i@her-cat.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HerCat\BaiduMap\WebApi\PlaceSearch;
 
 /**
@@ -12,26 +21,26 @@ interface PlaceSearchInterface
     /**
      * @param string $keyword
      * @param string $region
-     * @param array $options
+     * @param array  $options
      *
      * @return mixed
      */
     public function region($keyword, $region, $options = []);
 
     /**
-     * @param string $keyword
+     * @param string       $keyword
      * @param string|float $longitude
      * @param string|float $latitude
-     * @param array $options
+     * @param array        $options
      *
      * @return mixed
      */
     public function circle($keyword, $longitude, $latitude, $options = []);
 
     /**
-     * @param string $keyword
+     * @param string       $keyword
      * @param string|array $bounds
-     * @param array $options
+     * @param array        $options
      *
      * @return mixed
      */
@@ -39,8 +48,8 @@ interface PlaceSearchInterface
 
     /**
      * @param string|array $uid
-     * @param int $scope
-     * @param string $output
+     * @param int          $scope
+     * @param string       $output
      *
      * @return mixed
      */
