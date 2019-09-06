2aceec486c86a6f0afb0986c0e2c68c4bf856c76
her-cat <i@her-cat.com>
2019-09-06 23:29:25 +0800

    fix bug

--- src/Kernel/Support/File.php
+++ src/Kernel/Support/File.php
@@ -55,8 +55,10 @@ class File
      */
     public static function getStreamExt($stream)
     {
+        $ext = self::getExtBySignature($stream);
+
         try {
-            if (is_readable($stream)) {
+            if (empty($ext) && is_readable($stream)) {
                 $stream = file_get_contents($stream);
             }
         } catch (\Exception $e) {
@@ -66,7 +68,7 @@ class File
 
         $mime = strstr($fileInfo->buffer($stream), ';', true);
 
-        return isset(self::$extensionMap[$mime]) ? self::$extensionMap[$mime] : self::getExtBySignature($stream);
+        return isset(self::$extensionMap[$mime]) ? self::$extensionMap[$mime] : $ext;
     }
 
     /**
