9cd35e36cc38d807f6779f77dabb4782f522aa4d
her-cat <i@her-cat.com>
2019-09-19 15:56:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/WebApi/Direction/LiteClientTest.php
+++ tests/WebApi/Direction/LiteClientTest.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of the her-cat/baidu-map.
+ *
+ * (c) her-cat <i@her-cat.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace HerCat\BaiduMap\Tests\WebApi\Direction;
-
 
 use HerCat\BaiduMap\Kernel\ServiceContainer;
 use HerCat\BaiduMap\Tests\TestCase;
